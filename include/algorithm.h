b93e4c44bd94451340a48963022a338334e7c887
luwh0708 <luwh0708@thundersoft.com>
2020-11-03 10:00:45 +0800

    ADD: algorithm interface and source code

---
+++ include/algorithm.h
@@ -0,0 +1,7 @@
+#include "map_parser.h"
+
+// 评估一个局势，返回分数
+int evaluate(class server_data m, uint32_t &score);
+
+// 给定一个局势，执行算法
+int algorithm(class server_data m);
---
+++ src/algorithm.cpp
@@ -0,0 +1,13 @@
+#include "algorithm.h"
+
+int evaluate(class server_data m, uint32_t &score)
+{
+	// stub
+	return 0;
+}
+
+int algorithm(class server_data m)
+{
+	// stub
+	return 0;
+}
