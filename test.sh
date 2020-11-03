09071f744dc4be6af620669d8cbe23c6c8466b45
luwh0708 <luwh0708@thundersoft.com>
2020-11-03 10:17:22 +0800

    UPDATE: continuous test

--- test.sh
+++ test.sh
@@ -19,7 +19,7 @@ for file in out/*.test; do
 	filename=${filename%.test}
 	if [[ "$ret" != "0" ]]; then
 		echo -e "$filename: \033[31mfailed($ret)\033[0m"
-		exit 1
+		# exit 1
 	else
 		echo -e "$filename: \033[32msuccess\033[0m"
 	fi
